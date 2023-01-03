59818f8c4e6efe7606108566ca41d068fc686381
Marouane Faris <faris.marouane@gmail.com>
2023-01-03 20:01:02 +0100

    Fix global font not applied

--- pages/_app.js
+++ pages/_app.js
@@ -5,5 +5,14 @@ import { Merriweather } from '@next/font/google';
 const font = Merriweather({ subsets: ['latin'], weight: '400' });
 
 export default function App({ Component, pageProps }) {
-  return <Component className={font.className} {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${font.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />;
+    </>
+  );
 }
