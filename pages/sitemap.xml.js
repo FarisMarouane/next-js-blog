d8307f4ff171e38b8e09e629d552db2c81943ab0
Marouane Faris <faris.marouane@gmail.com>
2023-01-18 23:19:31 +0100

    Create sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,45 @@
+import { getAllArticles } from '../utils/mdx';
+
+function generateSiteMap(posts) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <url>
+       <loc>https://www.marouanefaris.dev</loc>
+     </url>
+     <url>
+       <loc>https://www.marouanefaris.dev/about_me</loc>
+     </url>
+     ${posts
+       .map(({ slug }) => {
+         return `
+       <url>
+           <loc>${`https://www.marouanefaris.dev/blog/${slug}`}</loc>
+       </url>
+     `;
+       })
+       .join('')}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  return;
+}
+
+export async function getServerSideProps({ res }) {
+  const posts = getAllArticles();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(posts);
+
+  res.setHeader('Content-Type', 'text/xml');
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
