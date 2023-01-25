3f72bacf316a07e361c8fe2cab6ac753eae5d516
Marouane Faris <faris.marouane@gmail.com>
2023-01-25 10:04:23 +0100

    Make sure footer is responsive in small screen

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from '../styles/components/Footer.module.css';
+import styles from '../styles/components/Footer.module.scss';
 
 const Footer = () => (
   <footer className={styles.footer}>
@@ -10,22 +10,26 @@ const Footer = () => (
     >
       Twitter
     </a>{' '}
-    &nbsp; &bull; &nbsp;
-    <a
-      href="https://github.com/FarisMarouane/next-js-blog"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Github
-    </a>{' '}
-    &nbsp; &bull; &nbsp;
-    <a
-      href="https://stackoverflow.com/users/6375543/faris-marouane"
-      target="_blank"
-      rel="noreferrer"
-    >
-      Stackoverflow
-    </a>
+    <div>
+      &nbsp; &bull; &nbsp;
+      <a
+        href="https://github.com/FarisMarouane/next-js-blog"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Github
+      </a>{' '}
+    </div>
+    <div>
+      &nbsp; &bull; &nbsp;
+      <a
+        href="https://stackoverflow.com/users/6375543/faris-marouane"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Stackoverflow
+      </a>
+    </div>
     <Link className={styles.resume} href="/about_me">
       About me
     </Link>
