da4a2ea0b3891be5f6fedc102d038ac930edd2e5
Marouane Faris <faris.marouane@gmail.com>
2023-04-03 17:30:49 +0200

    new blog design
    * Add new blog article
    
    * Revamp home page design
    
    * Change toggle button's focus and active shadow color
    
    * Change dark mode theme
    
    * Move all a links style to global css file
    
    * Style article interaction with click css events
    
    * Fix a link color error
    
    * Change theme color

--- components/Aside.tsx
+++ components/Aside.tsx
@@ -1,38 +1,30 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import styles from '../styles/components/Aside.module.css';
 
 const Aside = () => (
-  <aside>
-    <div className={styles.aside}>
-      <Image
-        className={styles.profile_pic}
-        src="/photo_linkedin.jpeg"
-        alt="marouane_faris_profile_pic"
-        width={56}
-        height={56}
-      />
-      <p className={styles.description}>
-        Personal blog by{' '}
-        <a
-          href="https://github.com/FarisMarouane/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Marouane Faris
-        </a>
-        .
-        <br />
-        Ideas expressed on this blog are stricly my own, and not those of
-        potential employers.
-        <br />
-        <br />
-        Made using the{' '}
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          Next JS framework
-        </a>
-      </p>
-    </div>
+  <aside className={styles.aside}>
+    <Image
+      className={styles.profile_pic}
+      src="/photo_linkedin.jpeg"
+      alt="marouane_faris_profile_pic"
+      width={56}
+      height={56}
+    />
+    <p>
+      Personal blog by <Link href="/about_me">Marouane Faris</Link>
+      .
+      <br />
+      Ideas expressed on this blog are stricly my own, and not those of
+      potential employers.
+      <br />
+      <br />
+      Made using the{' '}
+      <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
+        Next JS framework
+      </a>
+    </p>
   </aside>
 );
 
