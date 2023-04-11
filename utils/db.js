2c37ee44864a02cebfe6cd07dd4d6bf3725660c7
Marouane Faris <faris.marouane@gmail.com>
2023-04-11 22:18:06 +0200

    Create db query function

---
+++ utils/db.js
@@ -0,0 +1,14 @@
+import PG from 'pg';
+const pool = new PG.Pool();
+
+const query = async (text, params) => {
+  const start = Date.now();
+  const res = await pool.query(text, params);
+  const duration = Date.now() - start;
+  console.log('executed query', { text, duration, rows: res.rowCount });
+  return res;
+};
+
+const DB = { query };
+
+export default DB;
