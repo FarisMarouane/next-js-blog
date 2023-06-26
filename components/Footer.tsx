c82c64c430045ec0795ccfd3ac79d690701ab94d
Marouane Faris <faris.marouane@gmail.com>
2023-06-26 11:31:24 +0200

    Move about_me presentation to home page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,24 @@
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import styles from '../styles/components/Footer.module.scss';
 
-const Footer = () => (
-  <footer className={styles.footer}>
-    <Link className={styles.resume} href="/about_me">
-      About me
-    </Link>
-  </footer>
-);
+const Footer = () => {
+  const { pathname } = useRouter();
+  let route;
+
+  if (pathname === '/blog') {
+    route = { name: 'About me', path: '/' };
+  } else {
+    route = { name: 'Blog', path: '/blog' };
+  }
+
+  return (
+    <footer className={styles.footer}>
+      <Link className={styles.resume} href={route.path}>
+        {route.name}
+      </Link>
+    </footer>
+  );
+};
 
 export default Footer;
