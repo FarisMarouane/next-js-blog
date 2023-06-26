e340960468de144ed4a01157a1674cff7dbac9d3
Marouane Faris <faris.marouane@gmail.com>
2023-06-26 23:01:12 +0200

    Delete unused components

--- components/PostBody.tsx
+++
@@ -1,9 +0,0 @@
-const PostBody = ({ content }: { content: string }) => {
-  return (
-    <div>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
-    </div>
-  );
-};
-
-export default PostBody;
