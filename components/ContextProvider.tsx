274a39a79534ec043f72cace52ab7f024ad8bbb8
Marouane Faris <faris.marouane@gmail.com>
2023-07-05 14:53:28 +0200

    Delete no longer necessary language context

--- components/ContextProvider.tsx
+++ components/ContextProvider.tsx
@@ -15,44 +15,24 @@ export type ThemeContextType = {
 };
 
 export type LanguageType = 'eng' | 'fr' | undefined;
-export type LanguageContextType = {
-  language: LanguageType;
-  setLanguage: Dispatch<SetStateAction<LanguageType>>;
-};
 
 export const ThemeContext = createContext<ThemeContextType>({
   theme: 'light',
   setTheme: () => undefined,
 });
 
-export const LanguageContext = createContext<LanguageContextType>({
-  language: 'eng',
-  setLanguage: () => undefined,
-});
-
 const ContextProvider = ({ children }: { children: ReactElement }) => {
   const [theme, setTheme] = useState<ThemeType>(undefined);
-  const [language, setLanguage] = useState<LanguageType>(undefined);
 
   const themeContextValue = useMemo(() => ({ theme, setTheme }), [theme]);
-  const languageContextValue = useMemo(
-    () => ({ language, setLanguage }),
-    [language],
-  );
 
   useEffect(() => {
     if (theme) window.localStorage.setItem('color-scheme', theme);
   }, [theme]);
 
-  useEffect(() => {
-    if (language) window.localStorage.setItem('preferred-language', language);
-  }, [language]);
-
   return (
     <ThemeContext.Provider value={themeContextValue}>
-      <LanguageContext.Provider value={languageContextValue}>
-        {children}
-      </LanguageContext.Provider>
+      {children}
     </ThemeContext.Provider>
   );
 };
