7ccdfe8dca2267272231aa8aab5dbea32f89b571
Marouane Faris <faris.marouane@gmail.com>
2024-03-12 10:30:08 +0100

    Fix how service worker is registered

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,29 @@
-import Script from 'next/script';
-import { Merriweather } from 'next/font/google';
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import ContextProvider from '../components/ContextProvider';
-import Layout from '../components/Layout';
-import ErrorBoundary from '../components/ErrorBoundary';
-import '../styles/globals.css';
-import '../styles/components/Toggle.css';
+import Script from "next/script";
+import { Merriweather } from "next/font/google";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import ContextProvider from "../components/ContextProvider";
+import Layout from "../components/Layout";
+import ErrorBoundary from "../components/ErrorBoundary";
+import "../styles/globals.css";
+import "../styles/components/Toggle.css";
+import { useEffect } from "react";
 
-const font = Merriweather({ subsets: ['latin'], weight: '400' });
+const font = Merriweather({ subsets: ["latin"], weight: "400" });
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((serviceWorker) => {
+          console.log("Service Worker registered: ", serviceWorker);
+        })
+        .catch((error) => {
+          console.error("Error registering the Service Worker: ", error);
+        });
+    }
+  }, []);
   return (
     <>
       <Head>
@@ -28,7 +41,6 @@ export default function App({ Component, pageProps }: AppProps) {
               src="https://cdn.jsdelivr.net/npm/pwacompat"
               crossOrigin="anonymous"
             />
-            <Script src="/scripts.js" />
             <style jsx global>{`
               html {
                 font-family: ${font.style.fontFamily};
