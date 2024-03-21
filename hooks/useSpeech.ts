4e3771c71ab2d3f6a0ffce8213ac9582b80f461d
Marouane Faris <faris.marouane@gmail.com>
2024-03-21 05:11:09 +0100

    Take into account language change in speech synthesis feature

--- hooks/useSpeech.ts
+++ hooks/useSpeech.ts
@@ -1,14 +1,32 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const useSpeech = (text: string, locale: string | undefined) => {
   const [speak, setSpeak] = useState(false);
 
+  const utteranceRef = useRef<SpeechSynthesisUtterance | undefined>();
+  const localeRef = useRef<string | undefined>(locale);
+
   useEffect(() => {
     const speechSynthesis = window.speechSynthesis;
+    utteranceRef.current = new SpeechSynthesisUtterance(text);
+    utteranceRef.current.lang = locale === "fr" ? "fr-FR" : "en-US";
+
     if (!speechSynthesis) return;
 
-    const utterance = new SpeechSynthesisUtterance(text);
-    utterance.lang = locale === "fr" ? "fr-FR" : "en-US";
+    // Detect change in locale/language
+    if (
+      (locale === "fr" && localeRef.current !== "fr") ||
+      (locale === "en" && localeRef.current !== "en")
+    ) {
+      localeRef.current = locale;
+
+      // The two lines below need to be in this exact order, otherwise speecheSynthesis remains in a indefinite paused state
+      speechSynthesis.resume();
+      speechSynthesis.cancel(); // The cancel method by itself keeps the speechSynthesis in a paused state
+
+      setSpeak(false);
+      return;
+    }
 
     // Start speaking
     if (speak) {
@@ -17,7 +35,7 @@ const useSpeech = (text: string, locale: string | undefined) => {
         return;
       }
       speechSynthesis.cancel();
-      speechSynthesis.speak(utterance);
+      speechSynthesis.speak(utteranceRef.current);
     }
 
     // Pause speaking
