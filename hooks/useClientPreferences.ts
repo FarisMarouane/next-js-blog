4d95a3fef4485e9a52c60256a16582e01a6caf0d
Marouane Faris <faris.marouane@gmail.com>
2024-11-12 19:39:22 +0100

    Fix FOUC theme issue

--- hooks/useClientPreferences.ts
+++ hooks/useClientPreferences.ts
@@ -12,6 +12,9 @@ const useClientPreferences = () => {
     const matchMedia = window.matchMedia('(prefers-color-scheme: dark)');
     const bodyElement = document.querySelector('body');
 
+    // apply a visibility class to the body to ensure there's no FOUC
+    document.body.classList.add('body-visible');
+
     const listenToColorThemeChange = (event: MediaQueryListEvent) => {
       const isDarkMode = event.matches;
 
@@ -41,8 +44,11 @@ const useClientPreferences = () => {
       setTheme('dark');
     }
 
-    return () =>
-      matchMedia.removeEventListener('change', listenToColorThemeChange);
+    return () => {
+      // clean up by removing the class if the component unmounts
+      document.body.classList.remove('body-visible');
+      return matchMedia.removeEventListener('change', listenToColorThemeChange);
+    };
   }, [setTheme]);
 };
 
